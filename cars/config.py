00273e0fa909f1237a01d8a410d45c3a1c67c607
matveyvarlamov <matvejvarlamov495@gmail.com>
2025-03-03 09:47:28 +0700

    add parsers

--- cars/config.py
+++ cars/config.py
@@ -1,10 +1,16 @@
 
 
-class SITE_URLS: 
-    class KCar: 
+class SITE_URLS:
+    class KCar:
         catalog_url = "https://api.kcar.com/bc/timeDealCar/list"
         car_url = 'https://api.kcar.com/bc/car-info-detail-of-ng?i_sCarCd=EC61115610&i_sPassYn=N&bltbdKnd=CM050'
         cookies = {
             'ab.storage.deviceId.79570721-e48c-4ca4-b9d6-e036e9bfeff8': '%7B%22g%22%3A%22b7404a2c-510b-12a8-560e-95604a58f89a%22%2C%22c%22%3A1732187811554%2C%22l%22%3A1732187811554%7D',
             'ab.storage.sessionId.79570721-e48c-4ca4-b9d6-e036e9bfeff8': '%7B%22g%22%3A%220eb30e25-7578-9663-a8fb-77a6be7f7ac7%22%2C%22e%22%3A1732190300903%2C%22c%22%3A1732187811597%2C%22l%22%3A1732188500903%7D',
         }
+
+    class Mpark:
+        catalog_url = "https://api.m-park.co.kr/home/api/v1/wb/searchmycar/carlistinfo/get?"
+
+    class Charancha:
+        catalog_url = "https://charancha.com/v1/cars"
