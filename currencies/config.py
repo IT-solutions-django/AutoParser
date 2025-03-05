4e65415d90ccc287a26ed6eb6fb078ee868d2e6f
Пак Сергей <sergegnabri2005@gmail.com>
2025-03-05 15:06:00 +0700

    Fix parsing KRW

--- currencies/config.py
+++ currencies/config.py
@@ -4,4 +4,5 @@ class SETTINGS:
     JPY_URL = 'https://www.atb.su/services/exchange/' 
     CNY_URL = 'https://www.vtb.ru/api/currencyrates/table/lite?category=3&type=1'
     KRW_URL = 'https://solidbank.ru/currency-transactions/'
+    KRW_URL = 'https://solidbank.ru/api/v1/currency?action=getdata&city=ВЛАДИВОСТОК&curname=KRW'
     EUR_URL = 'https://auc.autocenter25.com/currency'
