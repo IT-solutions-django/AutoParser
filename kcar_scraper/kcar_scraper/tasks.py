32d1819623795d398b0cc7e5b01393aabc886b8d
matveyvarlamov <matvejvarlamov495@gmail.com>
2025-03-07 17:27:45 +0700

    Изменил путь tasks для scrapy

--- kcar_scraper/kcar_scraper/tasks.py
+++ kcar_scraper/kcar_scraper/tasks.py
@@ -1,9 +1,9 @@
 from celery import shared_task
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from kcar_scraper.spiders.kcar_spiders import KCarSpider
-from kcar_scraper.spiders.mpark_spiders import MparkSpider
-from kcar_scraper.spiders.charancha_spiders import CharanchaSpider
+from spiders.kcar_spiders import KCarSpider
+from spiders.mpark_spiders import MparkSpider
+from spiders.charancha_spiders import CharanchaSpider
 
 
 @shared_task
