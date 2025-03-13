aacb1c51cf212f4caa5ce25c3b09d30710d02670
matveyvarlamov <matvejvarlamov495@gmail.com>
2025-03-14 01:20:37 +0700

    Update import kcar_scraper tasks

--- core/celery.py
+++ core/celery.py
@@ -2,18 +2,26 @@ import os
 from celery import Celery 
 from celery.schedules import crontab
 from core.settings import CELERY_BROKER_URL
+import importlib
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings') 
 
 app = Celery(
     'core'
-) 
+)
+
 app.config_from_object('django.conf:settings', namespace='CELERY') 
 app.autodiscover_tasks()
-app.autodiscover_tasks(['kcar_scraper'])
 
+current_dir = os.getcwd()
 
+if 'kcar_scraper' in current_dir and current_dir.endswith('kcar_scraper'):
+    task_module = 'kcar_scraper.tasks'
+else:
+    task_module = 'kcar_scraper.kcar_scraper.tasks'
+
+importlib.import_module(task_module)
 
 app.conf.beat_schedule = {
     'update_jpy_daily': {
